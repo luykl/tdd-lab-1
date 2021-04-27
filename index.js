91d4e33809b84c8c664bc35718edb9981c94d57a
luykl <leah.logsin@gmail.com>
2021-04-27 15:34:05 -0400

    starting tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Use TDD practices to build a function that translates from English to Pig Latin
+
+function translate(string) {
+    let lowerCaseString = string.toLowerCase();
+    if (lowerCaseString[0] === "a" || lowerCaseString[0] === "e") {
+        return lowerCaseString+"way";
+    } else {
+        let firstLetter = lowerCaseString[0];
+        let newString = lowerCaseString.substr(1);
+        return newString + firstLetter + "ay";
+    }
+};
+
+module.exports = translate;
--- index.test.js
+++ index.test.js
@@ -1,6 +1,28 @@
+
+const translate = require("./index.js");
+
 // Starting tests:
 // Words starting with different vowels
 // translate("apple") → "applyway"
 // translate("else") → "elseway"
 // Words starting with one consonant
-// translate("giraffe") → "iraffegay"
+// translate("giraffe") → "iraffegay" 
+
+describe("translate function", () => {
+
+    test("test apple", () => {
+        let result = translate("apple");
+        expect(result).toEqual("appleway");
+    });
+
+    test("test else", () => {
+        let result = translate("else");
+        expect(result).toEqual("elseway");
+    });
+
+    test("test giraffe", () => {
+        let result = translate("giraffe");
+        expect(result).toEqual("iraffegay");
+    });
+
+});
