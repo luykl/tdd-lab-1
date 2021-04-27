a73dd1ce7ea9ae7d11bd9d18f3fd632661358a19
luykl <leah.logsin@gmail.com>
2021-04-27 15:11:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+//Use TDD practices to build a function that translates from English to Pig Latin
---
+++ index.test.js
@@ -0,0 +1,6 @@
+// Starting tests:
+// Words starting with different vowels
+// translate("apple") → "applyway"
+// translate("else") → "elseway"
+// Words starting with one consonant
+// translate("giraffe") → "iraffegay"
