26fb40c4b2278aa09134a69b285832a9a9f06c82
luykl <leah.logsin@gmail.com>
2021-04-27 16:02:20 -0400

    passing tests

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 function translate(string) {
     let lowerCaseString = string.toLowerCase();
-    if (lowerCaseString[0] === "a" || lowerCaseString[0] === "e") {
+    if (lowerCaseString[0] === "a" || lowerCaseString[0] === "e" || lowerCaseString[0] === "i" || lowerCaseString[0] === "o" || lowerCaseString[0] === "u") {
         return lowerCaseString+"way";
-    } else {
+    } else if (lowerCaseString[1] === "a" || lowerCaseString[1] === "e" || lowerCaseString[1] === "i" || lowerCaseString[1] === "o" || lowerCaseString[1] === "u") {
         let firstLetter = lowerCaseString[0];
         let newString = lowerCaseString.substr(1);
         return newString + firstLetter + "ay";
+    } else  if (lowerCaseString[2] === "a" || lowerCaseString[2] === "e" || lowerCaseString[2] === "i" || lowerCaseString[2] === "o" || lowerCaseString[2] === "u"){
+        let firstTwoLetters = lowerCaseString[0] + lowerCaseString[1];
+        let newString = lowerCaseString.substr(2);
+        return newString + firstTwoLetters + "ay";
+    } else {
+        let firstThreeLetters = lowerCaseString[0] + lowerCaseString[1] + lowerCaseString[2];
+        let newString = lowerCaseString.substr(3);
+        return newString + firstThreeLetters + "ay";
     }
 };
 
--- index.test.js
+++ index.test.js
@@ -1,12 +1,21 @@
 
+const { test, expect } = require("@jest/globals");
 const translate = require("./index.js");
 
-// Starting tests:
+// STARTING TESTS:
 // Words starting with different vowels
 // translate("apple") → "applyway"
 // translate("else") → "elseway"
 // Words starting with one consonant
 // translate("giraffe") → "iraffegay" 
+
+//ADDITIONAL TESTS
+// At least five (5) tests for words starting with different vowels.
+// At least two (2) tests for words starting with one consonant.
+// At least one (1) test for words starting with two consonants.
+// At least one (1) test for words starting with three consonants.
+// At least one (1) test checking for lower case conversion.
+
 
 describe("translate function", () => {
 
@@ -24,5 +33,68 @@ describe("translate function", () => {
         let result = translate("giraffe");
         expect(result).toEqual("iraffegay");
     });
+
+    test("starts with a", () => {
+        let result = translate("after");
+        expect(result).toEqual("afterway");
+    });
+
+    test("starts with e", () => {
+        let result = translate("empire");
+        expect(result).toEqual("empireway");
+    });
+    
+    test("starts with i", () => {
+        let result = translate("igloo");
+        expect(result).toEqual("iglooway");
+    });
+    
+    test("starts with o", () => {
+        let result = translate("omelet");
+        expect(result).toEqual("omeletway");
+    });
+    
+    test("starts with u", () => {
+        let result = translate("upside");
+        expect(result).toEqual("upsideway");
+    });
+
+    test("starts with y", () => {
+        let result = translate("yellow");
+        expect(result).toEqual("ellowyay")
+    });
+    
+    test("starts one consonant", () => {
+        let result = translate("consonant");
+        expect(result).toEqual("onsonantcay");
+    });
+    
+    test("starts with another consonant", () => {
+        let result = translate("result");
+        expect(result).toEqual("esultray");
+    });
+
+    test("check for lowercase", () => {
+        let result = translate("Purple");
+        expect(result).toEqual("urplepay");
+    });
+
+    test("starts with two consonants", () => {
+        let result = translate("play");
+        expect(result).toEqual("ayplay");
+    });
+
+    test("another starts with two consonants", () => {
+        let result = translate("stick");
+        expect(result).toEqual("ickstay");
+    });
+
+    test("another starts with three consonants", () => {
+        let result = translate("throw");
+        expect(result).toEqual("owthray");
+    });
+
+
+
 
 });
